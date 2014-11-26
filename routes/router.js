e781ad46e5e0cda51c637add45805344c6c0ec45
MasterOfPoppets <gurrkin@googlemail.com>
2014-11-26 10:22:29 +0000

    Redo delete keys...

--- routes/router.js
+++ routes/router.js
@@ -2,16 +2,15 @@
   'use strict';
   
   var Contact = require('../lib/contact'),
-      Recaptcha = require('recaptcha').Recaptcha,
-      keys = require('../data/recaptcha.json');
+      Recaptcha = require('recaptcha').Recaptcha;
   
   exports.blogEntry = function (req, res) {
     res.render('partials/blogEntry'); 
   };
   
   exports.contact = function (req, res) {
-    var PUBLIC_KEY = process.env.RECAPTCHA_PUBLIC_KEY || keys.publickey,
-        SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY || keys.secretkey,
+    var PUBLIC_KEY = process.env.RECAPTCHA_PUBLIC_KEY,
+        SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY,
         recaptcha = new Recaptcha(PUBLIC_KEY, SECRET_KEY);
     
     res.render('partials/contact', {
