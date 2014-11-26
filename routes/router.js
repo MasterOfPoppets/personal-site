87d2e88d5819c23701f8a72bc0f7481fd3cb4a0c
MasterOfPoppets <gurrkin@googlemail.com>
2014-11-26 10:17:20 +0000

    Undo delete keys
    Heroku...

--- routes/router.js
+++ routes/router.js
@@ -2,15 +2,16 @@
   'use strict';
   
   var Contact = require('../lib/contact'),
-      Recaptcha = require('recaptcha').Recaptcha;
+      Recaptcha = require('recaptcha').Recaptcha,
+      keys = require('../data/recaptcha.json');
   
   exports.blogEntry = function (req, res) {
     res.render('partials/blogEntry'); 
   };
   
   exports.contact = function (req, res) {
-    var PUBLIC_KEY = process.env.RECAPTCHA_PUBLIC_KEY,
-        SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY,
+    var PUBLIC_KEY = process.env.RECAPTCHA_PUBLIC_KEY || keys.publickey,
+        SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY || keys.secretkey,
         recaptcha = new Recaptcha(PUBLIC_KEY, SECRET_KEY);
     
     res.render('partials/contact', {
