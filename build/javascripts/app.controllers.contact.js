207ee04085a4305849097bdf20a3e5069d2d4fcf
MasterOfPoppets <gurrkin@googlemail.com>
2014-12-19 10:58:14 +0000

    Update gbst portfolio
    Also, first play with Famo.us

--- build/javascripts/app.controllers.contact.js
+++ build/javascripts/app.controllers.contact.js
@@ -5,16 +5,18 @@
   angular.module('gh.controller.contact', [])
   
     .controller('ContactCtrl', [
-      '$scope', 
-      '$http',
-      'PageFactory', 
-      function ($scope, $http, PageFactory) {
+      '$scope', '$http', 'PageFactory', '$famous',
+      function ($scope, $http, PageFactory, $famous) {
+//        var Transitionable = $famous['famous/transitions/Transitionable'];
+//        var Easing = $famous['famous/transitions/Easing'];
+        
         $scope.formData = {};
         $scope.submitted = false;
+//        $scope.opacity = new Transitionable(0);
         
         PageFactory.newPage('Contact | Gareth Hughes');
-      
-        $scope.submit = function(contactForm) {
+        
+        $scope.submit = function (contactForm) {
           $scope.submitted = true;
           $http({
             method: 'POST',
@@ -22,6 +24,15 @@
             data: $scope.formData
           });
         };
+        
+//        $scope.animate = function () {
+//          $scope.opacity.set(1, {
+//            duration: 500,
+//            curve: 'easeInOut'
+//          });
+//        };
+//        
+//        $scope.animate();
       }
     ]);
 }());
--- build/javascripts/app.controllers.portfolio.js
+++ build/javascripts/app.controllers.portfolio.js
@@ -5,23 +5,29 @@
   angular.module('gh.controller.portfolio', [])
   
     .controller('PortfolioCtrl', [
-      '$scope', 'PageFactory', 
-      function ($scope, PageFactory) {
+      '$scope', 'PageFactory', '$famous',
+      function ($scope, PageFactory, $famous) {
+        var Transitionable = $famous['famous/transitions/Transitionable'];
+        var Easing = $famous['famous/transitions/Easing'];
+        
+        $scope.angle = new Transitionable(0);
+        
         $scope.isShowPortfolioItem = PageFactory.showExpandedItem;
+        $scope.test = '/images/portfolio/syn_logo.png';
         $scope.portfolioLinks = [
-          {
-            link: 'gbst-syn',
-            image: '/images/portfolio/syn_logo.png'
-          },
-          {
-            link: 'ombudz',
-            image: '/images/portfolio/syn_logo.png'
-          },
           {
             link: 'gbst-syn',
             image: '/images/portfolio/syn_logo.png'
           }
         ];
+        
+        $scope.animate = function () {
+          console.log('clicked');
+          $scope.angle.set(2 * Math.PI, {
+            duration: 1000,
+            curve: 'easeInOut'
+          });
+        };
 
         PageFactory.newPage('Portfolio | Gareth Hughes');
     }])
