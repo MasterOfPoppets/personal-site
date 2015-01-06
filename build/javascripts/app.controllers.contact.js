c3a1c9f0a4f9587a74c2756854a80dc303416bb1
MasterOfPoppets <gurrkin@googlemail.com>
2015-01-06 14:58:07 +0000

    Update contact
    On success, prior to implementing some famo.us

--- build/javascripts/app.controllers.contact.js
+++ build/javascripts/app.controllers.contact.js
@@ -4,35 +4,63 @@
   'use strict';
   angular.module('gh.controller.contact', [])
   
-    .controller('ContactCtrl', [
-      '$scope', '$http', 'PageFactory', '$famous',
-      function ($scope, $http, PageFactory, $famous) {
-//        var Transitionable = $famous['famous/transitions/Transitionable'];
-//        var Easing = $famous['famous/transitions/Easing'];
-        
-        $scope.formData = {};
-        $scope.submitted = false;
-//        $scope.opacity = new Transitionable(0);
-        
-        PageFactory.newPage('Contact | Gareth Hughes');
-        
-        $scope.submit = function (contactForm) {
-          $scope.submitted = true;
-          $http({
-            method: 'POST',
-            url: '/contact',
-            data: $scope.formData
-          });
-        };
-        
-//        $scope.animate = function () {
-//          $scope.opacity.set(1, {
-//            duration: 500,
-//            curve: 'easeInOut'
-//          });
-//        };
-//        
-//        $scope.animate();
+  .controller('ContactCtrl', [
+    '$scope', '$famous', '$state', 'PageFactory',
+    function ($scope, $famous, $state, PageFactory) {
+      var Transitionable = $famous['famous/transitions/Transitionable'];
+      var Easing = $famous['famous/transitions/Easing'];
+
+      // If we are here through a refresh then we will not be in the correct
+      // state, so we will manually transition to it here.
+      if (!$state.is('contact.form')) {
+        $state.transitionTo('contact.form');
       }
-    ]);
+
+      $scope.formData = {};
+      $scope.submitted = false;
+      $scope.opacity = new Transitionable(0);
+
+      PageFactory.newPage('Contact | Gareth Hughes');
+
+      $scope.animate = function () {
+        $scope.opacity.set(1, {
+          duration: 500,
+          curve: 'easeInOut'
+        });
+      };
+    }
+  ])
+
+  .controller('ContactFormCtrl', [
+    '$scope', '$http', '$state',
+    function ($scope, $http, $state) {
+      $scope.submit = function (contactForm) {
+        $scope.submitted = true;
+        $http({
+          method: 'POST',
+          url: '/contact',
+          data: $scope.formData
+        })
+        .success(function () {
+          $state.transitionTo('contact.success');
+        });
+      };
+    }
+  ])
+  
+  .controller('ContactSuccessCtrl', [
+    '$scope', '$famous',
+    function ($scope, $famous) {
+      var Transitionable = $famous['famous/transitions/Transitionable'];
+      var Easing = $famous['famous/transitions/Easing'];
+      $scope.opacity = new Transitionable(0);
+      
+      $scope.animate = function () {
+        $scope.opacity.set(1, {
+          duration: 500,
+          curve: 'easeInOut'
+        });
+      }();
+    }
+  ]);
 }());
--- build/javascripts/app.js
+++ build/javascripts/app.js
@@ -6,63 +6,71 @@
   angular.module('GHApp', [
     'GHControllers', 'GHFactories', 'famous.angular', 'ui.router'])
   
-    .config([
-      '$stateProvider', '$urlRouterProvider', '$locationProvider',
-      function ($stateProvider, $urlRouterProvider, $locationProvider) {
-        $urlRouterProvider.otherwise('/home');
-        $locationProvider.html5Mode(true);
-        $stateProvider
-          .state('home', {
-            url: '/home',
-            templateUrl: 'partials/home',
-            controller: 'HomeCtrl'
-          })
-          .state('skills', {
-            url: '/skills',
-            templateUrl: 'partials/skills',
-            controller: 'SkillsCtrl'
-          })
-          .state('portfolio', {
-            url: '/portfolio',
-            templateUrl: 'partials/portfolio',
-            controller: 'PortfolioCtrl'
-          })
-          .state('portfolio.item', {
-            url: '/{id}',
-            templateUrl: function ($stateParams) {
-              return 'partials/portfolio/' + $stateParams.id;
-            },
-            controller: 'PortfolioItemCtrl'
-          })
-          .state('play', {
-            url: '/play',
-            templateUrl: 'partials/play',
-            controller: 'PlayCtrl'
-          })
-          .state('play.item', {
-            url: '/{id}',
-            templateUrl: function ($stateParams) {
-              return 'partials/play/' + $stateParams.id;
-            },
-            controller: 'PlayItemCtrl'
-          })
-          .state('blog', {
-            url: '/blog',
-            templateUrl: 'partials/blog',
-            controller: 'BlogCtrl'
-          })
-          .state('blogItem', {
-            url: '/blog/{id}',
-            templateUrl: function ($stateParams) {
-              return 'partials/blog/' + $stateParams.id; 
-            },
-            controller: 'BlogCtrl'
-          })
-          .state('contact', {
-            url: '/contact',
-            templateUrl: 'partials/contact',
-            controller: 'ContactCtrl'
-          });
-      }
-    ]);
+  .config([
+    '$stateProvider', '$urlRouterProvider', '$locationProvider',
+    function ($stateProvider, $urlRouterProvider, $locationProvider) {
+      $urlRouterProvider.otherwise('/home');
+      $locationProvider.html5Mode(true);
+      $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: 'partials/home',
+        controller: 'HomeCtrl'
+      })
+      .state('skills', {
+        url: '/skills',
+        templateUrl: 'partials/skills',
+        controller: 'SkillsCtrl'
+      })
+      .state('portfolio', {
+        url: '/portfolio',
+        templateUrl: 'partials/portfolio',
+        controller: 'PortfolioCtrl'
+      })
+      .state('portfolio.item', {
+        url: '/{id}',
+        templateUrl: function ($stateParams) {
+          return 'partials/portfolio/' + $stateParams.id;
+        },
+        controller: 'PortfolioItemCtrl'
+      })
+      .state('play', {
+        url: '/play',
+        templateUrl: 'partials/play',
+        controller: 'PlayCtrl'
+      })
+      .state('play.item', {
+        url: '/{id}',
+        templateUrl: function ($stateParams) {
+          return 'partials/play/' + $stateParams.id;
+        },
+        controller: 'PlayItemCtrl'
+      })
+      .state('blog', {
+        url: '/blog',
+        templateUrl: 'partials/blog',
+        controller: 'BlogCtrl'
+      })
+      .state('blogItem', {
+        url: '/blog/{id}',
+        templateUrl: function ($stateParams) {
+          return 'partials/blog/' + $stateParams.id;
+        },
+        controller: 'BlogCtrl'
+      })
+      .state('contact', {
+        url: '/contact',
+        templateUrl: 'partials/contact',
+        controller: 'ContactCtrl'
+      })
+      .state('contact.form', {
+        templateUrl: 'partials/contact/contact_form',
+        controller: 'ContactFormCtrl'
+      })
+      .state('contact.success', {
+        templateUrl: 'partials/contact/contact_success',
+        controller: 'ContactSuccessCtrl'
+      });
+    }
+  ]);
 }());
