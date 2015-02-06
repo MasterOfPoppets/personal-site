3266d50525c6450b86420e5a6a1732cd54cb30a1
MasterOfPoppets <gurrkin@googlemail.com>
2015-02-06 08:59:27 +0000

    [Update] Directives
    Turned flakey jQuery anchor scroller into directive - should now be
    possible to use anchor scrolling from within templates meaning I can
    change the navigation when I leave the page!

--- build/javascripts/app.directives.js
+++ build/javascripts/app.directives.js
@@ -3,6 +3,21 @@
 (function () {
   'use strict';
   angular.module('gh.directives', [])
+  
+  .directive('ghScrollingAnchor', function () {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        element.on('click', function (event) {
+          $('html, body').stop().animate({
+            scrollTop: $(attrs.href).offset().top
+          }, 1500, 'easeInOutExpo');
+          
+          event.preventDefault();
+        });
+      }
+    };
+  })
 
   .directive('ghSocialLink', function () {
     return {
