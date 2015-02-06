7c2b47846b80e780bd24802c01ef6021f0a6f6ae
MasterOfPoppets <gurrkin@googlemail.com>
2015-02-06 12:14:51 +0000

    [Grunt] Minify
    Minify bower javascripts for faster loading.

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,8 +14,11 @@
             'famous-angular-bower'
           ],
           mainFiles: {
+            'angular': 'angular.min.js',
+            'angular-ui-router': 'angular-ui-router.min.js',
             'bootstrap': ['dist/css/bootstrap.css', 'dist/js/bootstrap.min.js'],
             'famous': 'dist/famous-global.js',
+            'jquery': 'dist/jquery.min.js',
             'marked': 'lib/marked.js'
           }
         }
