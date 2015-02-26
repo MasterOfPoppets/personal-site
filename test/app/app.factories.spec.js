8cc052ea57ce1090c61992efa69e8e9289786ef9
MasterOfPoppets <gurrkin@googlemail.com>
2015-02-26 15:47:05 +0000

    [Update] PageFactory testing

--- test/app/app.factories.spec.js
+++ test/app/app.factories.spec.js
@@ -1,20 +1,40 @@
-var should = chai.should();
+var should = chai.should()
 
 describe('PageFactory', function () {
   var PageFactory
 
   beforeEach(module('gh.factories'))
   beforeEach(inject(function (_PageFactory_) {
-    PageFactory = _PageFactory_;
+    PageFactory = _PageFactory_
   }))
 
-  it('should be all good now', function (done) {
-    should.not.exist(null);
-    done();
+  it('should have some default settings', function () {
+    PageFactory.title().should.equal('Gareth Hughes')
+    PageFactory.showExpandedItem().should.be.false
   })
 
-  it('should be all good now', function (done) {
-    should.exist(null);
-    done();
+  describe('#newPage()', function () {
+    beforeEach(function () {
+      PageFactory.setShowExpandedItem(true);
+      PageFactory.newPage('Test')
+    })
+
+    it('should change the title', function () {
+      PageFactory.title().should.equal('Test')
+    })
+
+    it('should reset the expansion flag', function () {
+      PageFactory.showExpandedItem().should.be.false
+    })
   })
-});
+
+  describe('#setShowExpandedItem()', function () {
+    beforeEach(function () {
+      PageFactory.setShowExpandedItem(true);
+    })
+
+    it('should change the expansion flag', function () {
+      PageFactory.showExpandedItem().should.be.true
+    })
+  })
+})
