2b1f8b2435ff9f5231084c35a22db128da44ccea
Gareth Hughes <gurrkin@googlemail.com>
2015-03-12 21:32:08 +0000

    [Chore] Small tidy up using WebStorm

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,79 +1,79 @@
 (function () {
-  'use strict';
-  module.exports = function (grunt) {
-    // Project configuration.
-    grunt.initConfig({
-      bower_concat: {
-        all: {
-          dest: 'public/javascripts/_bower.js',
-          cssDest: 'public/stylesheets/_bower.css',
-          exclude: [
-            'famous',
-            'famous-angular',
-            'famous-angular-bower'
-          ],
-          mainFiles: {
-            'angular': 'angular.min.js',
-            'angular-ui-router': 'release/angular-ui-router.min.js',
-            'bootstrap': ['dist/css/bootstrap.css', 'dist/js/bootstrap.min.js'],
-            'famous': 'dist/famous-global.js',
-            'marked': 'lib/marked.js'
-          }
-        }
-      },
-      karma: {
-        unit: {
-          configFile: 'karma.conf.js',
-          background: true
-        }
-      },
-      simplemocha: {
-        options: {
-          reporter: 'spec',
-          ui: 'bdd'
-        },
-        all: {
-          src: ['test/**/*.js', '!test/**/*.spec.js']
-        }
-      },
-      uglify: {
-        options: {
-          mangle: false
-        },
-        my_target: {
-          files: {
-            'public/javascripts/gh.min.js': ['build/javascripts/**/*.js']
-          }
-        }
-      },
-      watch: {
-        bower: {
-          files: ['bower_components/**'],
-          tasks: ['bower_concat']
-        },
-        client_js: {
-          files: ['build/javascripts/**/*.js'],
-          tasks: ['uglify', 'karma:unit:run']
-        },
-        client_js_test: {
-          files: ['test/**/*.spec.js'],
-          tasks: ['karma:unit:run']
-        },
-        server_js_test: {
-          files: ['routes/**/*.js', 'lib/**/*.js', 'test/**/*.js', '!test/**/*.spec.js'],
-          tasks: ['simplemocha']
-        }
-      }
-    });
+    'use strict';
+    module.exports = function (grunt) {
+        // Project configuration.
+        grunt.initConfig({
+            bower_concat: {
+                all: {
+                    dest: 'public/javascripts/_bower.js',
+                    cssDest: 'public/stylesheets/_bower.css',
+                    exclude: [
+                        'famous',
+                        'famous-angular',
+                        'famous-angular-bower'
+                    ],
+                    mainFiles: {
+                        'angular': 'angular.min.js',
+                        'angular-ui-router': 'release/angular-ui-router.min.js',
+                        'bootstrap': ['dist/css/bootstrap.css', 'dist/js/bootstrap.min.js'],
+                        'famous': 'dist/famous-global.js',
+                        'marked': 'lib/marked.js'
+                    }
+                }
+            },
+            karma: {
+                unit: {
+                    configFile: 'karma.conf.js',
+                    background: true
+                }
+            },
+            simplemocha: {
+                options: {
+                    reporter: 'spec',
+                    ui: 'bdd'
+                },
+                all: {
+                    src: ['test/**/*.js', '!test/**/*.spec.js']
+                }
+            },
+            uglify: {
+                options: {
+                    mangle: false
+                },
+                my_target: {
+                    files: {
+                        'public/javascripts/gh.min.js': ['build/javascripts/**/*.js']
+                    }
+                }
+            },
+            watch: {
+                bower: {
+                    files: ['bower_components/**'],
+                    tasks: ['bower_concat']
+                },
+                client_js: {
+                    files: ['build/javascripts/**/*.js'],
+                    tasks: ['uglify', 'karma:unit:run']
+                },
+                client_js_test: {
+                    files: ['test/**/*.spec.js'],
+                    tasks: ['karma:unit:run']
+                },
+                server_js_test: {
+                    files: ['routes/**/*.js', 'lib/**/*.js', 'test/**/*.js', '!test/**/*.spec.js'],
+                    tasks: ['simplemocha']
+                }
+            }
+        });
 
-    // Load the plugins
-    grunt.loadNpmTasks('grunt-bower-concat');
-    grunt.loadNpmTasks('grunt-karma');
-    grunt.loadNpmTasks('grunt-simple-mocha');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+        // Load the plugins
+        grunt.loadNpmTasks('grunt-bower-concat');
+        grunt.loadNpmTasks('grunt-karma');
+        grunt.loadNpmTasks('grunt-simple-mocha');
+        grunt.loadNpmTasks('grunt-contrib-uglify');
+        grunt.loadNpmTasks('grunt-contrib-watch');
 
-    // Default task(s).
-    grunt.registerTask('default', ['karma:unit:start', 'watch']);
-  };
+        // Default task(s).
+        grunt.registerTask('default', ['karma:unit:start', 'watch']);
+    };
 }());
--- test/app/app.factories.spec.js
+++ test/app/app.factories.spec.js
@@ -1,40 +1,40 @@
 var should = chai.should()
 
 describe('PageFactory', function () {
-  var PageFactory
+    var PageFactory
 
-  beforeEach(module('gh.factories'))
-  beforeEach(inject(function (_PageFactory_) {
-    PageFactory = _PageFactory_
-  }))
+    beforeEach(module('gh.factories'))
+    beforeEach(inject(function (_PageFactory_) {
+        PageFactory = _PageFactory_
+    }))
 
-  it('should have some default settings', function () {
-    PageFactory.title().should.equal('Gareth Hughes')
-    PageFactory.showExpandedItem().should.be.false
-  })
-
-  describe('#newPage()', function () {
-    beforeEach(function () {
-      PageFactory.setShowExpandedItem(true);
-      PageFactory.newPage('Test')
+    it('should have some default settings', function () {
+        PageFactory.title().should.equal('Gareth Hughes')
+        PageFactory.showExpandedItem().should.be.false
     })
 
-    it('should change the title', function () {
-      PageFactory.title().should.equal('Test')
+    describe('#newPage()', function () {
+        beforeEach(function () {
+            PageFactory.setShowExpandedItem(true);
+            PageFactory.newPage('Test')
+        })
+
+        it('should change the title', function () {
+            PageFactory.title().should.equal('Test')
+        })
+
+        it('should reset the expansion flag', function () {
+            PageFactory.showExpandedItem().should.be.false
+        })
     })
 
-    it('should reset the expansion flag', function () {
-      PageFactory.showExpandedItem().should.be.false
-    })
-  })
+    describe('#setShowExpandedItem()', function () {
+        beforeEach(function () {
+            PageFactory.setShowExpandedItem(true);
+        })
 
-  describe('#setShowExpandedItem()', function () {
-    beforeEach(function () {
-      PageFactory.setShowExpandedItem(true);
+        it('should change the expansion flag', function () {
+            PageFactory.showExpandedItem().should.be.true
+        })
     })
-
-    it('should change the expansion flag', function () {
-      PageFactory.showExpandedItem().should.be.true
-    })
-  })
 })
